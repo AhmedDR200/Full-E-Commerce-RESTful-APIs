a28a2f6682bc10fa1ec635cb94594803e4d081d4
AhmedDR200 <alshwwhy212@gmail.com>
2023-12-05 15:53:06 +0200

    Logging and ENV Variables

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+dotenv.config();
+
 const app = express();
 
+// Morgan Middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 
 
@@ -15,7 +23,7 @@ app.get('/', (req, res) => {
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
+    console.log(`server (${process.env.NODE_ENV}) listening at http://localhost:${port}`)
 });
