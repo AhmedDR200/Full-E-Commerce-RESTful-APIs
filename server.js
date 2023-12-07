7af89f9eaf7f30377b58864c74f91feda3873501
AhmedDR200 <alshwwhy212@gmail.com>
2023-12-07 13:53:11 +0200

    Handle Unhandled Routes and Send Error to Error Handling Middleware

--- server.js
+++ server.js
@@ -13,18 +13,36 @@ mongoose.connect(process.env.DB_URL)
 .catch(err => console.log(err));
 
 
+// Body Parser Middleware
+app.use(express.json());
+
 
 // Morgan Middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Body Parser Middleware
-app.use(express.json());
-
 
 // Routes
 app.use('/cats', catsRoutes);
+
+
+// 404 Error Handling Middleware
+app.all('*', (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
+});
+
+
+// Global Error Handling Middleware
+app.use((err, req, res, next) => {
+    res.status(400).json({
+        success: false,
+        error: err.message
+    });
+});
 
 
 
