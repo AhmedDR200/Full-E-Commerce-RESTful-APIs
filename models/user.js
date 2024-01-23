0d8fb8087c772631516270c65c17e646095939c0
AhmedDR200 <alshwwhy212@gmail.com>
2024-01-23 23:31:39 +0200

    User Model setup

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        trim: true,
+        required: [true, "name required"]
+    },
+    slug:{
+        type: String,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: [true, "email required"],
+        unique: true,
+        lowercase: true
+    },
+    phone:String,
+    profileImg:String,
+
+    password: {
+        type: String,
+        required: [true, "password required"],
+        minlength: [6, "Too short password"]
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+},{
+    timestamps: true,
+    versionKey: false
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
