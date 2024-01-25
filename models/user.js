0f76161a073939569a82f2da475a7b82a5a56e71
AhmedDR200 <alshwwhy212@gmail.com>
2024-01-25 13:14:27 +0200

    Hash User Password On Create Handler

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -36,6 +37,13 @@ const userSchema = new mongoose.Schema({
 },{
     timestamps: true,
     versionKey: false
+});
+
+// Hashing Password => Mongoose Schema
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
