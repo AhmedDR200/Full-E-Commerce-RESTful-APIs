5a28196e659b90745d6154a1294d2fb086347f7e
AhmedDR200 <alshwwhy212@gmail.com>
2024-02-01 22:21:30 +0200

    Coupon Schema & Model

---
+++ models/coupon.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const couponSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Coupon name is required'],
+        unique: [true, 'Coupon name must be unique'],
+    },
+    discount: {
+        type: Number,
+        required: [true, 'Coupon discount is required'],
+    },
+    expiry: {
+        type: Date,
+        required: [true, 'Coupon expiry date is required'],
+    }
+}, {
+    versionKey: false,
+    timestamps: true,
+});
+
+const Coupon = mongoose.model('Coupon', couponSchema);
+
+module.exports = Coupon;
