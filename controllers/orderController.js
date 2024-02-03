2ecbacac740f888265aed3a59b6304d61b3faf2d
AhmedDR200 <alshwwhy212@gmail.com>
2024-02-03 19:38:27 +0200

    Edits

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,6 +4,7 @@ const factory = require('./handelers');
 const Order = require('../models/order');
 const Cart = require('../models/cart');
 const Product = require('../models/product');
+const User = require('../models/user');
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -177,9 +178,44 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 });
 
 
+const createCardOrder = async (session) => {
+    const cartId = session.client_reference_id;
+    const shippingAddress = session.metadata;
+    const oderPrice = session.amount_total / 100;
+  
+    const cart = await Cart.findById(cartId);
+    const user = await User.findOne({ email: session.customer_email });
+  
+    // 3) Create order with default paymentMethodType card
+    const order = await Order.create({
+      user: user._id,
+      cartItems: cart.cartItems,
+      shippingAddress,
+      totalOrderPrice: oderPrice,
+      isPaid: true,
+      paidAt: Date.now(),
+      paymentMethodType: 'card',
+    });
+  
+    // 4) After creating order, decrement product quantity, increment product sold
+    if (order) {
+      const bulkOption = cart.cartItems.map((item) => ({
+        updateOne: {
+          filter: { _id: item.product },
+          update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
+        },
+      }));
+      await Product.bulkWrite(bulkOption, {});
+  
+      // 5) Clear cart depend on cartId
+      await Cart.findByIdAndDelete(cartId);
+    }
+};
+
+
 // @desc    This webhook will run when stripe payment success paid
 // @route   POST /webhook-checkout
-// @access  Protected/User
+// @access  Private/Auth User
 exports.webhookCheckout = asyncHandler(async (req, res, next) => {
     const sig = req.headers['stripe-signature'];
   
